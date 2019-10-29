9a295180b6e1635e6dd7bedea0d7ae706e409616
haomengwudaydayup <49704773+haomengwudaydayup@users.noreply.github.com>
2019-10-30 00:00:07 +0800

    栈接口 -  自制

---
+++ BinaryTree/Stack.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include"BTNodeTree.h"
+
+
+//栈接口 -- 为方便以后调用方便所开辟设计的
+/*
+	采取线性表实现栈操作。
+*/
+
+typedef BTNode* SDataType;
+
+typedef struct Stack
+{
+	SDataType* _data;		//指针
+	size_t _size;				//大小
+	size_t _memory;		//当前的容量
+}Stack;
+
+//初始化栈
+void StackInit(Stack* s);
+//栈插入
+void StackPush(Stack* s, SDataType x);
+//栈删除
+void StackPop(Stack* s);
+//栈顶元素
+SDataType StackTop(Stack* s);
+//栈大小
+int StackSize(Stack* s);
+//判断栈是否为空
+int StackEmpty(Stack* s);
+//判断栈满
+int StackOver(Stack* s);
+//栈销毁
+void StackDestroy(Stack* s);
